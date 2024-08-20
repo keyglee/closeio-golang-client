149a4b60c3ba98bb2fea74161b8187687f8e800b
Ryley Lamb <ryleylamb@gmail.com>
2024-08-20 11:45:42 -0700

    Creating test case for creating and then deleting the lead

--- closeio/create_test.go
+++ closeio/create_test.go
@@ -1,6 +1,8 @@
 package closeio_test
 
 import (
+	"encoding/json"
+	"os"
 	"testing"
 
 	"github.com/keyglee/closeio-golang-client/closeio"
@@ -8,14 +10,33 @@ import (
 )
 
 func TestCreateLead(t *testing.T) {
-
+	apiKey := os.Getenv("CLOSE_IO_API_KEY")
 	newLead := lead.Lead{Name: "Name", Description: "Some Description"}
 
-	client := closeio.CreateCloseClient("SomeApiKey")
+	client := closeio.CreateCloseClient(apiKey)
 	bytes, err := client.Create(newLead)
 
 	if err != nil {
 		t.Error(err)
 	}
+	t.Log(string(bytes))
+
+	var response lead.LeadResponse
+
+	err = json.Unmarshal(bytes, &response)
+
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	t.Log(response)
+
+	deleteLead := lead.Lead{ID: response.ID}
+	bytes, err = client.Delete(deleteLead)
+
+	if err != nil {
+		t.Error(err)
+	}
+
 	t.Log(string(bytes))
 }
